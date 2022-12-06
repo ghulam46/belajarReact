1189ab29907c2d5e74767af97a3e86a31a590d05
Ghulam Ammar Yanuar <65816301+ghulam46@users.noreply.github.com>
2022-12-06 15:31:50 +0700

    add class component

---
+++ src/Component/Class/HeaderClass.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+class HeaderClass extends React.Component {
+    render() {
+        return (
+         <div>
+            <h1>Ini adalah Header Class</h1>
+         </div>   
+        )
+    }
+}
+
+export default HeaderClass;
